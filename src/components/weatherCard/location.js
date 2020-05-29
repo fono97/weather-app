39479877bf5f7459406e3fc564a453a80203ec2a
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-05-29 08:51:12 -0400

    I added the first card

---
+++ src/components/weatherCard/location.js
@@ -0,0 +1,25 @@
+import React from "react "
+import styled from "@emotion/styled";
+
+const Location = (props) => {
+
+    const Container = styled.div`
+    text-align:center;
+    `
+    const City = styled.h1`
+      font-family: "Merriweather", sans-serif;
+      font-size: 1.6em;
+    `
+       const Country = styled.h3`
+         font-family: "Fira sans", sans-serif;
+         font-size: 1.1rem;
+       `
+    return (
+      <Container>
+        < City>Sydney</City>
+        < Country >AU</Country>
+      </Container>
+    );
+}
+ 
+export default Location;
