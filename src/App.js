276e98e5d88c4d3528446388d943edf050ba3b4b
gabriel andy fonogit config --global user.email fonoandy <fonoandy@gmail.com>
2020-05-30 15:42:54 -0400

    I used the lifecycle method and useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './App.css';
 
 import WeatherCard from './components/component';
 
+
 function App() {
+  const[query,setQuery] = useState("gabon")
+  const [weather, setWeather] = useState({
+    temp:null,
+    city:null,
+    condition:null,
+    country:null
+  });
+  const data = async (q) =>{
+    const apiRes = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${q},gb&APPID=239540bf8f08676bf711b3f51ce1f1a6`);
+    const resJSON = await apiRes.json();
+    return resJSON
+  };
+  const handleSearch = e =>{
+    e.preventDefault();
+    data(query).then(res =>{
+      setWeather({
+        temp: res.main.temp,
+        city: res.name,
+        condition: res.weather[0].main,
+        country: res.country
+      })
+    });
+  };
+  useEffect(() =>{
+    data(query).then(res =>{
+      setWeather({
+        temp:res.main.temp,
+        city:res.name,
+        condition:res.weather[0].main,
+        country:res.country
+      })
+    })
+  },[]);
   return (
     <div className="App">
-      <WeatherCard temp = {-5} condition ="clear" country ="AU"/>
-      <WeatherCard temp={40} condition="Clouds" country="AU"/>
-      <WeatherCard temp={-30} condition="Tornado" country="GB" />
+      <WeatherCard 
+      temp ={weather.temp}
+      condition={weather.condition}
+      city={weather.city}
+      country = {weather.country}
+      />
+      <form>
+        <input value = {query} onChange = {e =>setQuery(e.target.value)}/>
+      <button onclick = {e => handleSearch(e)}>Search</button>
+      </form>
     </div>
   )
 }
